3cbf7a5680cd2204ad513a0c4877384a630db913
Xitong Gao <gxtfmx@gmail.com>
2013-09-24 00:29:01 +0100

    Add clear sky and daytime support

--- .external/weather.py
+++ .external/weather.py
@@ -21,20 +21,32 @@ def fetch(location, celcius=True):
 
 
 def pictograph(json_str):
+    def is_daytime():
+        from datetime import datetime
+        return 6 <= datetime.now().hour < 18
     _pictograph_dict = {
-        2: '☈',   # thunderstorm
-        3: '☂',   # drizzle
-        4: '☔',   # rain
-        6: '❄',   # snow
-        7: '〰',  # mist/smoke/haze/sand/fog
-        8: '☁',   # clouds
-        9: '颶',  # extreme
+        2: '☈',    # thunderstorm
+        3: '☂',    # drizzle
+        4: '☔',    # rain
+        6: '❄',    # snow
+        7: '〰',   # mist/smoke/haze/sand/fog
+        8: '☁',    # clouds
+        9: '颶',   # extreme
+        # specials
+        800: '☽☼'  # clear sky
     }
-    code = int(json_str['weather'][0]['id'] / 100)
+    code = json_str['weather'][0]['id']
     try:
-        return _pictograph_dict[code]
+        pict = _pictograph_dict[code]
+    except KeyError:
+        code = int(code / 100)
+    try:
+        pict = _pictograph_dict[code]
     except KeyError:
         return '?'
+    if len(pict) == 1:
+        return pict
+    return pict[is_daytime()]
 
 
 def temperature(json_str):
@@ -50,6 +62,7 @@ def format_weather(location, celcius=True, prec=0):
     return '{pictograph}{temperature:.{prec}f}{unit}'.format(
         pictograph=pictograph(json_str), prec=prec,
         temperature=temperature(json_str), unit=unit)
+
 
 if __name__ == '__main__':
     import sys
