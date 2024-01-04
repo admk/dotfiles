7576005346f0c582a4f4ed35725a74270164c3f6
Xitong Gao <gxtfmx@gmail.com>
2024-01-05 04:01:50 +0800

    Update install script

--- .config/xonsh/as_default.sh
+++ .config/xonsh/as_default.sh
@@ -10,28 +10,34 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 export PATH="$HOME/.local/bin:$HOME/.conda/bin:$PATH"
 
+cd $HOME
+
 # conda
+CONDA_URL=https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda
 CONDA_PREFIX=$HOME/.miniconda3
 if [ ! -f "$CONDA_PREFIX/bin/conda" ]; then
-    echo "Downloading miniconda3..."
-    curl -L -O https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    echo "Installing miniconda3..."
-    sh Miniconda3-latest-Linux-x86_64.sh -b -p $CONDA_PREFIX > /dev/null
-    rm Miniconda3-latest-Linux-x86_64.sh
+    mkdir -p $XDG_CACHE_HOME
+    CONDA_INSTALLER=$XDG_CACHE_HOME/miniconda3.sh
+    echo "Downloading miniconda3 to $CONDA_INSTALLER..."
+    curl -L $CONDA_URL/Miniconda3-latest-Linux-x86_64.sh -o $CONDA_INSTALLER
+    echo "Installing miniconda3 to $CONDA_PREFIX..."
+    sh $CONDA_INSTALLER -u -b -p $CONDA_PREFIX > /dev/null
+    if [ -f $CONDA_INSTALLER ]; then
+        rm $CONDA_INSTALLER
+    fi
 fi
 
 # xonsh
-CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CONDA_PYTHON=$CONDA_PREFIX/bin/python
 if [[ -z $XONSH_VERSION ]]; then
-    cd $HOME
     if ! $CONDA_PYTHON -c 'import xonsh' &>/dev/null; then
-        echo "Installing xonsh..."
+        USER_BASE=$($CONDA_PYTHON -m site --user-base)
+        echo "Installing xonsh to $USER_BASE..."
         $CONDA_PYTHON -m pip install --user pip
-        $CONDA_PYTHON -m pip install --user -r $CURR_DIR/requirements.txt
+        $CONDA_PYTHON -m pip install --user -r $XDG_CONFIG_HOME/xonsh/requirements.txt
     fi
     if ! $CONDA_PYTHON -c 'import xonsh' &>/dev/null; then
-        echo 'Xonsh failed to install.'
+        echo 'Xonsh failed to install, fall back to $SHELL.'
         exec $SHELL
     else
         $CONDA_PREFIX/bin/python -m xonsh
