f4e5352ee5ef066e71f74977919d3f116072dd7d
Xitong Gao <gxtfmx@gmail.com>
2024-01-04 13:59:42 +0800

    .

--- .config/xonsh/as_default.sh
+++ .config/xonsh/as_default.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export $HOME="~/.kodot"
-export $XDG_CONFIG_HOME=$HOME/.config
-export $XDG_DATA_HOME=$HOME/.local/share
-export $XDG_CACHE_HOME=$HOME/.cache
+export HOME="~/.kodot"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
 
 if [[ -z $XONSH_VERSION ]]; then
     if ! command -v xonsh &>/dev/null; then
