4c52977a2c7d5a2e850627672a4bee9cbf3630bc
Xitong Gao <gxtfmx@gmail.com>
2024-01-04 13:40:58 +0800

    .

--- .config/xonsh/as_default.sh
+++ .config/xonsh/as_default.sh
@@ -4,7 +4,7 @@ CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [[ -z $XONSH_VERSION ]]; then
     if ! command -v xonsh &>/dev/null; then
         echo "Installing xonsh..."
-        pip install --user -r $CURR_DIR/../requirements.txt
+        pip install --user -r $CURR_DIR/requirements.txt
     fi
     python -m xonsh
 fi
