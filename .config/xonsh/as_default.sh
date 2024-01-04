7af2fcdae7a7668e4517fe4c2e888db2144ac412
Xitong Gao <gxtfmx@gmail.com>
2024-01-04 12:25:41 +0800

    Another attemp at xsh

---
+++ .config/xonsh/as_default.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ ! -z $XONSH_VERSION ]]; then
+    if ! command -v xonsh &>/dev/null; then
+        echo "Installing xonsh..."
+        pip install --user -r .config/xonsh/requirements.txt
+    fi
+    exec xonsh
+fi
