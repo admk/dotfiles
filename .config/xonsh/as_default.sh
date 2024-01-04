01d8c09c1a166a36a3bf8f9c7ea78c25e8026729
Xitong Gao <gxtfmx@gmail.com>
2024-01-04 13:56:56 +0800

    .

--- .config/xonsh/as_default.sh
+++ .config/xonsh/as_default.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export $HOME=~/.kodot
+export $HOME="~/.kodot"
 export $XDG_CONFIG_HOME=$HOME/.config
 export $XDG_DATA_HOME=$HOME/.local/share
 export $XDG_CACHE_HOME=$HOME/.cache
@@ -11,5 +11,6 @@ if [[ -z $XONSH_VERSION ]]; then
         echo "Installing xonsh..."
         pip install --user -r $CURR_DIR/requirements.txt
     fi
+    cd $HOME
     python -m xonsh
 fi
