3ba8952982cc211690920d766015b89085dc3dd2
Xitong Gao <238821+admk@users.noreply.github.com>
2024-07-02 14:05:06 +0800

    Add untracked nvim config files

---
+++ .config/nvim/lua/plugins/complete.lua
@@ -0,0 +1,71 @@
+return {
+    {
+        "zbirenbaum/copilot-cmp",
+        dependencies = "copilot.lua",
+        config = function()
+            require("copilot").setup({
+                suggestion = { enabled = false },
+                pane = { enabled = false },
+            })
+            require("copilot_cmp").setup()
+        end,
+    },
+    {
+        "nvim-cmp",
+        dependencies = {
+            "zbirenbaum/copilot-cmp",
+        },
+        opts = function(_, opts)
+            -- local has_words_before = function()
+            --     if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+            --         return false
+            --     end
+            --     unpack = unpack or table.unpack
+            --     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            --     local text = vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]
+            --     return col ~= 0 and text:match("^%s*$") == nil
+            -- end
+            local cmp = require("cmp")
+            opts.sources = cmp.config.sources({
+                { name = "copilot" },
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "path" },
+            })
+            opts.mapping = vim.tbl_extend("force", opts.mapping, {
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        local entry = cmp.get_selected_entry()
+                        if not entry then
+                            cmp.select_next_item({
+                                behavior = cmp.SelectBehavior.Select,
+                            })
+                        else
+                            cmp.confirm()
+                        end
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<C-n>"] = cmp.mapping(function(_)
+                    if cmp.visible() then
+                        cmp.select_next_item({
+                            behavior = cmp.SelectBehavior.Select,
+                        })
+                    end
+                end),
+                ["<C-p>"] = cmp.mapping(function(_)
+                    if cmp.visible() then
+                        cmp.select_prev_item({
+                            behavior = cmp.SelectBehavior.Select,
+                        })
+                    end
+                end),
+                ["<C-e>"] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Replace,
+                }),
+            })
+            return opts
+        end,
+    },
+}
---
+++ .config/nvim/lua/plugins/languages.lua
@@ -0,0 +1,87 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "cpp",
+                "gitignore",
+                "go",
+                "http",
+                "latex",
+                "python",
+                "rust",
+                "typst",
+            },
+            query_linter = {
+                enable = true,
+                use_virtual_text = true,
+                lint_events = { "BufWrite", "CursorHold" },
+            },
+        },
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "stylua",
+                "selene",
+                "luacheck",
+                "shellcheck",
+                "shfmt",
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        init = function()
+            local keys = require("lazyvim.plugins.lsp.keymaps").get()
+            keys[#keys + 1] = {
+                "gd",
+                function()
+                    require("telescope.builtin").lsp_definitions({
+                        reuse_win = false,
+                    })
+                end,
+                desc = "Goto Definition",
+                has = "definition",
+            }
+        end,
+        opts = {
+            autoformat = false,
+            inlay_hints = { enabled = false },
+            use_virtual_text = false,
+            servers = {
+                pyright = {
+                    settings = {
+                        python = {
+                            pythonPath = vim.fn.exepath("python"),
+                        },
+                    },
+                },
+                yamlls = {
+                    settings = {
+                        yaml = { keyOrdering = false },
+                    },
+                },
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            diagnostics = { globals = { "vim", "require" } },
+                        },
+                    },
+                },
+            },
+        },
+    },
+    {
+        "rachartier/tiny-inline-diagnostic.nvim",
+        dependencies = { "neovim/nvim-lspconfig" },
+        event = "VeryLazy",
+        config = function()
+            vim.diagnostic.config({
+                virtual_text = false,
+            })
+            require('tiny-inline-diagnostic').setup()
+        end
+    }
+}
