f292fb01357b06da9cdecca91c6eeae08ecafede
Xitong Gao <238821+admk@users.noreply.github.com>
2024-09-06 23:11:22 +0800

    navigate

--- .config/nvim/lua/plugins/navigate.lua
+++ .config/nvim/lua/plugins/navigate.lua
@@ -4,6 +4,47 @@ return {
         enabled = false,
     },
     {
+        "echasnovski/mini.nvim",
+        version = false,
+        keys = {
+            {
+                "<leader>m",
+                function () require("mini.files").open() end,
+                desc = "Open mini.files (current file)",
+            },
+            {
+                "<leader>M",
+                function ()
+                    require("mini.files").open(vim.fn.expand("%:p:h"))
+                end,
+                desc = "Open mini.files (CWD)",
+            },
+        },
+        config = function(_, opts)
+            local tabnew_open = function()
+                local new_target_window
+                local cur_target_window = require("mini.files").get_target_window()
+                if cur_target_window ~= nil then
+                    vim.api.nvim_win_call(cur_target_window, function()
+                        vim.cmd("tabnew")
+                        new_target_window = vim.api.nvim_get_current_win()
+                    end)
+                    require("mini.files").set_target_window(new_target_window)
+                    require("mini.files").go_in({ close_on_file = true })
+                end
+            end
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "MiniFilesBufferCreate",
+                callback = function(args)
+                    local buf_id = args.data.buf_id
+                    vim.keymap.set(
+                        "n", "t", tabnew_open,
+                        { buffer = buf_id, desc = "Open in new tab" })
+                end,
+            })
+        end,
+    },
+    {
         "mikavilpas/yazi.nvim",
         event = "VeryLazy",
         keys = {
