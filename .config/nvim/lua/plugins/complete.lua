35bd211e288fb6b54c4792ff44bcd7bda28bfe67
Xitong Gao <238821+admk@users.noreply.github.com>
2025-07-01 02:16:37 +0800

    nvim: minor

--- .config/nvim/lua/plugins/complete.lua
+++ .config/nvim/lua/plugins/complete.lua
@@ -7,42 +7,16 @@ return {
             keymap = {
                 preset = "super-tab",
             },
+            signature = { window = { border = 'rounded' } },
             completion = {
                 accept = {
-                    auto_brackets = {
-                        enabled = false,
-                    },
+                    auto_brackets = { enabled = false },
                 },
+            },
+            fuzzy = {
+                max_typos = function(_) return 0 end,
+                sorts = { 'exact', 'score', 'sort_text' },
             },
         },
     },
-    -- {
-    --     "yetone/avante.nvim",
-    --     event = "VeryLazy",
-    --     lazy = true,
-    --     keys = {
-    --         { "<leader>a", mode = { "n", "v" }, desc = "+Avante" },
-    --     },
-    --     opts = {
-    --         provider = "openai",
-    --         auto_suggestions_provider = "openai",
-    --         openai = {
-    --             endpoint = "https://api.deepseek.com/v1",
-    --             model = "deepseek-chat",
-    --             timeout = 30000, -- Timeout in milliseconds
-    --             temperature = 0,
-    --             max_tokens = 4096,
-    --             ["local"] = false,
-    --         },
-    --     },
-    --     build = "make",
-    --     dependencies = {
-    --         "nvim-treesitter/nvim-treesitter",
-    --         "stevearc/dressing.nvim",
-    --         "nvim-lua/plenary.nvim",
-    --         "MunifTanjim/nui.nvim",
-    --         "nvim-tree/nvim-web-devicons",
-    --         "zbirenbaum/copilot.lua",
-    --     },
-    -- },
 }
