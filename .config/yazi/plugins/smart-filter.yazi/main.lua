086b0afdb44788f40a17e8aafa0b570964b16b07
Xitong Gao <238821+admk@users.noreply.github.com>
2025-07-08 00:46:50 +0800

    yazi: Update plugins

--- .config/yazi/plugins/smart-filter.yazi/main.lua
+++ .config/yazi/plugins/smart-filter.yazi/main.lua
@@ -1,4 +1,4 @@
---- @since 25.2.26
+--- @since 25.5.31
 
 local hovered = ya.sync(function()
 	local h = cx.active.current.hovered
@@ -16,7 +16,8 @@ end)
 local function prompt()
 	return ya.input {
 		title = "Smart filter:",
-		position = { "center", w = 50 },
+		pos = { "center", w = 50 },
+		position = { "center", w = 50 }, -- TODO: remove
 		realtime = true,
 		debounce = 0.1,
 	}
@@ -28,20 +29,20 @@ local function entry()
 	while true do
 		local value, event = input:recv()
 		if event ~= 1 and event ~= 3 then
-			ya.mgr_emit("escape", { filter = true })
+			ya.emit("escape", { filter = true })
 			break
 		end
 
-		ya.mgr_emit("filter_do", { value, smart = true })
+		ya.emit("filter_do", { value, smart = true })
 
 		local h = hovered()
 		if h.unique and h.is_dir then
-			ya.mgr_emit("escape", { filter = true })
-			ya.mgr_emit("enter", {})
+			ya.emit("escape", { filter = true })
+			ya.emit("enter", {})
 			input = prompt()
 		elseif event == 1 then
-			ya.mgr_emit("escape", { filter = true })
-			ya.mgr_emit(h.is_dir and "enter" or "open", { h.url })
+			ya.emit("escape", { filter = true })
+			ya.emit(h.is_dir and "enter" or "open", { h.url })
 		end
 	end
 end
