45975da409607df4d8a3da8ba44aef25a38f362d
Xitong Gao <238821+admk@users.noreply.github.com>
2026-03-04 11:07:52 +0800

    nvim: remove color detect

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,40 +1,5 @@
 local is_remote = os.getenv("SSH_CLIENT") or os.getenv("SSH_TTY")
 
-local function set_color()
-  local color_file = vim.env.XDG_CONFIG_HOME .. "/kxh/share/color"
-  if vim.fn.filereadable(color_file) == 0 then
-    return
-  end
-  local color_mode = vim.split(vim.fn.readfile(color_file)[1], ":")[1]
-  if not color_mode then
-    color_mode = os.getenv("KXH_COLOR_MODE")
-  end
-  if not color_mode then
-    return
-  end
-  color_mode = vim.split(color_mode, ":")[1]
-  color_mode = color_mode and color_mode:lower()
-  if not color_mode then
-    return
-  end
-  if vim.o.background ~= color_mode then
-    vim.o.background = color_mode
-    -- vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-  end
-end
-vim.api.nvim_create_autocmd("VimEnter", {
-  pattern = "*",
-  callback = function() set_color() end,
-})
-if not is_remote then
-  -- timer for color change detect
-  local timer = vim.loop.new_timer()
-  if not timer then
-    error("Failed to create timer for color change detection")
-  end
-  timer:start(10000, 10000, vim.schedule_wrap(set_color))
-end
-
 local color = "catppuccin"
 local colorschemes = {
   catppuccin = {
