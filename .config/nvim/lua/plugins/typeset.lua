c8a6694370f24f8aefa4c34fb008443a045f2706
Xitong Gao <238821+admk@users.noreply.github.com>
2026-03-07 15:12:07 +0800

    nvim: jieba broken

--- .config/nvim/lua/plugins/typeset.lua
+++ .config/nvim/lua/plugins/typeset.lua
@@ -98,25 +98,5 @@ return {
       'noearc/jieba.nvim',
       enabled = is_local,
       ft = { "markdown" },
-      dependencies = {'noearc/jieba-lua'},
-      config = function(_, opts)
-        vim.api.nvim_create_autocmd("FileType", {
-          pattern = "markdown",
-          callback = function()
-            local flags = {noremap = false, silent = true, buffer = true}
-            vim.keymap.set({'x', 'n'}, 'B', '<cmd>lua require("jieba_nvim").wordmotion_B()<CR>', flags)
-            vim.keymap.set({'x', 'n'}, 'b', '<cmd>lua require("jieba_nvim").wordmotion_b()<CR>', flags)
-            vim.keymap.set({'x', 'n'}, 'w', '<cmd>lua require("jieba_nvim").wordmotion_w()<CR>', flags)
-            vim.keymap.set({'x', 'n'}, 'W', '<cmd>lua require("jieba_nvim").wordmotion_W()<CR>', flags)
-            vim.keymap.set({'x', 'n'}, 'E', '<cmd>lua require("jieba_nvim").wordmotion_E()<CR>', flags)
-            vim.keymap.set({'x', 'n'}, 'e', '<cmd>lua require("jieba_nvim").wordmotion_e()<CR>', flags)
-            vim.keymap.set({'x', 'n'}, 'ge', '<cmd>lua require("jieba_nvim").wordmotion_ge()<CR>', flags)
-            vim.keymap.set({'x', 'n'}, 'gE', '<cmd>lua require("jieba_nvim").wordmotion_gE()<CR>', flags)
-            vim.keymap.set('n', 'ce', ":lua require'jieba_nvim'.change_w()<CR>", flags)
-            vim.keymap.set('n', 'de', ":lua require'jieba_nvim'.delete_w()<CR>",  flags)
-            vim.keymap.set('n', '<leader>w' , ":lua require'jieba_nvim'.select_w()<CR>", flags)
-        end,
-      })
-      end,
   },
 }
